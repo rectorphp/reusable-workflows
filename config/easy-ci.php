1dd5d5e7e61828ad21e411d63f6aa0578256ba2b
Tomas Votruba <tomas.vot@gmail.com>
2021-11-02 23:07:51 +0100

    skip types

--- config/easy-ci.php
+++ config/easy-ci.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Core\Contract\Rector\RectorInterface;
+use Rector\Set\Contract\SetListInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCI\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::TYPES_TO_SKIP, [
-         RectorInterface::class
+        RectorInterface::class,
+        SetListInterface::class,
     ]);
 };
